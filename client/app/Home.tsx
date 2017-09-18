20ba853da83b021416ae3225812172b804e7f58d
Deepa Ramam <dshenoy@gmail.com>
2017-09-18 09:06:28 -0700

    updated home

--- client/app/Home.tsx
+++ client/app/Home.tsx
@@ -7,16 +7,17 @@ const Home = () => {
   };
   const buttonStyle = {
     textDecoration: 'none',
-    color: 'black'
+    color: 'black',
+    marginRight: '10px'
   };
   return (
     <div style={groupStyle}>
-      <button type='button' className='btn btn-warning btn-lg' style={{marginRight: '10px'}}>
-        <a href='/groups' style={buttonStyle}>I'm hungry. Let's eat.</a>
-      </button>
-      <button type='button' className='btn btn-warning btn-lg'>
-        <a href='/addEmployee' style={buttonStyle}>New hire. Join the group.</a>
-      </button>
+      <a className='btn btn-warning btn-lg' style={buttonStyle} href='/groups'>
+        I'm hungry. Let's eat.
+      </a>
+      <a className='btn btn-warning btn-lg' style={buttonStyle} href='/addEmployee'>
+        New hire. Join the group.
+      </a>
     </div>
   );
 };
